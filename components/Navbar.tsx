7726c7d0e5c53aebb66a1b19a8b5a1c07f06dd73
MudasirTech786 <m.mudasirasfi@gmail.com>
2026-02-17 16:39:58 +0500

    changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,7 +16,7 @@ export default function Navbar() {
 
         <div className="space-x-8 font-medium">
           <Link href="/">Home</Link>
-          <Link href="/products">Products</Link>
+          <Link href="/products">Product</Link>
           <Link href="/about">About</Link>
           <Link href="/contact">Contact</Link>
         </div>
